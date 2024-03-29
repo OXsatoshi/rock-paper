95f4dfd585c0b0962af3f65bf76b49225c2efb25
OXsatoshi <afia.nabil24@gmail.com>
2024-03-29 06:13:27 +0100

    adding the ui

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 const availabaleChoices=["rock","scissors","paper"];
+//the first index is the score of player 
+//seconde index is the score of computer
+const score = [0,0];
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3);
    
     return availabaleChoices[computerChoice];
 }
-function playerSelection(playerChoice,computerChoice){
+//this function return 1 when player win 
+//return 2 when computer win 
+//return 0 if Draw
+function playRound(playerChoice){
+    const computerChoice = getComputerChoice();
+    console.log(computerChoice);
     let playerIndex = transferPlayerChoiceToIndex(playerChoice);
     let computerIndex = transferPlayerChoiceToIndex(computerChoice);
-    if(playerIndex == computerIndex) return "draw";
+    if(playerIndex == computerIndex) return 0;
     if(playerIndex == 2 || computerIndex == 2){
         if(computerIndex == 2) {
-            if(playerIndex == 1) return "player win ";
-            if(playerIndex == 0) return "computer win";
+            if(playerIndex == 1) return 1;
+            if(playerIndex == 0) return 2;
         }
         if(playerIndex == 2){
-            if(computerIndex == 1) return "computer win";
-            if(computerIndex == 0) return "player win";
+            if(computerIndex == 1) return 2;
+            if(computerIndex == 0) return 1;
         }
 
 
     }
     else {
         if(playerIndex < computerIndex)
-            return "player win ";
-        else return "computer win";
+            return 1;
+        else return 2;
     }
 }
 function transferPlayerChoiceToIndex(stringChoice){
@@ -33,15 +41,48 @@ function transferPlayerChoiceToIndex(stringChoice){
         }
     }
 }
-function playGame(){
-    let playerChoice;
-    let computerChoice;
-    for(let i = 0;i<5;i++){
-        playerChoice = prompt("enter your choice ?");
-        computerChoice = getComputerChoice();
-        console.log(playerChoice);
-        console.log(computerChoice);
-        console.log(playerSelection(playerChoice,computerChoice));
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+const playerScore = document.querySelector(".player-score");
+const computerScore = document.querySelector(".computer-score");
+const winner = document.querySelector(".winner");
+rockBtn.addEventListener("click",()=>{
+    let winnerOfAround = playRound("rock");
+    if(winnerOfAround !==0){
+        score[winnerOfAround-1]++;
+        playerScore.textContent = score[0];
+        computerScore.textContent = score[1];
+        if(score[0] == 5) winner.textContent = "Player";
+        if(score[1] == 5) winner.textContent = "Computer";
+    
     }
 }
-playGame();
+);
+paperBtn.addEventListener("click",()=>{
+    let winnerOfAround = playRound("paper");
+    if(winnerOfAround !==0){
+        score[winnerOfAround-1]++;
+        playerScore.textContent = score[0];
+        computerScore.textContent = score[1];
+        if(score[0] == 5) winner.textContent = "Player";
+        if(score[1] == 5) winner.textContent = "Computer";
+    
+    }
+});
+scissorsBtn.addEventListener("click",()=>{
+    let winnerOfAround = playRound("scissors");
+    if(winnerOfAround !==0){
+        score[winnerOfAround-1]++;
+        playerScore.textContent = score[0];
+        computerScore.textContent = score[1];
+        if(score[0] == 5) winner.textContent = "Player";
+        if(score[1] == 5) winner.textContent = "Computer";
+    
+    }
+})
+function handleClick(playerChoice){
+    console.log("hi");
+    
+}    
+
